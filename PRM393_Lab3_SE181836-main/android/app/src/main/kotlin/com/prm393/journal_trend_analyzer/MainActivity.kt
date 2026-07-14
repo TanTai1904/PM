a6683271d88b3b5d2ac59c36eba64066d6ce360f
Tan Tai <taitqtse182652@gmail.com>
2026-07-14 09:01:48 +0700

    222

--- PRM393_Lab3_SE181836-main/android/app/src/main/kotlin/com/prm393/journal_trend_analyzer/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.prm393.journal_trend_analyzer
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- PRM393_Lab3_SE181836-main/lib/models/author_stat.dart
+++
@@ -1,8 +0,0 @@
-/// Represents an author with publication count statistics.
-class AuthorStat {
-  const AuthorStat({required this.id, required this.name, required this.count});
-
-  final String id;
-  final String name;
-  final int count;
-}
